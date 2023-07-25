786a4856311f7513eab186fc9bf339d65d34225c
Raghupathi <raghupathi.jan.01@gmail.com>
2023-07-25 21:21:24 +0530

    authentication set

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 const express = require('express')
 const connectDB = require('./db/connect')
+const cookieParser = require('cookie-parser')
+const seesion = require('express-session')
 const passport = require('passport')
 
 // Load User Model
@@ -21,7 +23,23 @@ app.get('/', (req, res) => {
     res.send('It Works!')
 })
 
+app.use(cookieParser());
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitalized: false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use((req, res, next) => {
+res.locals.user = req.user || null;
+next();
+});
+
 app.use('/auth', auth)
+
 
 const port = process.env.PORT || 5000;
 
